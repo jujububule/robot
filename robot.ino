cf2d6899bd80ea2964bace4ff12ed151f4957440
jujububule <julien.talent01@gmail.com>
2024-01-25 18:29:01 +0100

    Update robot.ino

--- robot.ino
+++ robot.ino
@@ -1,3 +1,11 @@
+/*
+Ce code a été écrit en 2023 par Julien Talent et les membres de son groupe en 1ère STI2D 3.
+Ce code est celui qui aura fait fonctionner le robot du lycée Adam de Craponne à Salon de Provence.
+Je souhaite remercier tout mes professeurs et mes camarades de classe pour leurs aide et leurs soutiens.
+    Merci !
+https://github.com/jujububule/robot
+*/
+
 #define dirMotoD 7
 #define vitMotoD 6
 
@@ -19,18 +27,24 @@ Servo servo;
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("starup");
+  Serial.println("Startup !");
+  
   //pin moteur droit
   pinMode(dirMotoD, OUTPUT);
   pinMode(vitMotoD, OUTPUT);
+  
   //pin moteur gauche
   pinMode(dirMotoG, OUTPUT);
   pinMode(vitMotoG, OUTPUT);
+  
   //pin servo moteur
   servo.attach(servoPin);
+
   //pin capteur ultrason
   pinMode(ultraTrig, OUTPUT);
   pinMode(ultraEcho, INPUT);
+
+  Serial.println("Started !");
 }
 
 void loop()
@@ -55,7 +69,7 @@ void loop()
    }
    else
    {
-    Serial.println("demi-tour");
+    Serial.println("Fait Demi-tour !");
     gauche(vitesseMoto); delay(tempQuartTour*2);
     stop(); 
    }
@@ -64,58 +78,75 @@ void loop()
 
 void reculer(int vitesse)
 {
+  Serial.println("Recule !");
+// Pour faire tourner les moteurs dans le bon sens 
   digitalWrite(dirMotoD,HIGH);
   digitalWrite(dirMotoG,HIGH);
+// Pour donner un acoup et lancer les moteurs
   analogWrite(vitMotoD, 255);
   analogWrite(vitMotoG, 255);
   delay(5);
+// Pour faire tourner les moteurs a la vitesse voulue
   analogWrite(vitMotoD, vitesse);
   analogWrite(vitMotoG, vitesse);
 }
 
 void avancer(int vitesse)
 {
+  Serial.println("Avance !");
+// Pour faire tourner les moteurs dans le bon sens 
   digitalWrite(dirMotoD,LOW);
   digitalWrite(dirMotoG,LOW);
+// Pour donner un acoup et lancer les moteurs
   analogWrite(vitMotoD, 255);
   analogWrite(vitMotoG, 255);
   delay(5);
+// Pour faire tourner les moteurs a la vitesse voulue
   analogWrite(vitMotoD, vitesse);
   analogWrite(vitMotoG, vitesse);
 }
 
 void droite(int vitesse)
 {
-  Serial.println("droite");
+  Serial.println("Tourne a Droite !");
+// Pour faire tourner les moteurs dans le bon sens 
   digitalWrite(dirMotoD,HIGH);
   digitalWrite(dirMotoG,LOW);
+// Pour donner un acoup et lancer les moteurs
   analogWrite(vitMotoD, 255);
   analogWrite(vitMotoG, 255);
   delay(5);
+// Pour faire tourner les moteurs a la vitesse voulue
   analogWrite(vitMotoD, vitesse);
   analogWrite(vitMotoG, vitesse);
 }
 
 void gauche(int vitesse)
 {
-  Serial.println("gauche");
+  Serial.println("Tourne a Gauche !");
+// Pour faire tourner les moteurs dans le bon sens 
   digitalWrite(dirMotoD,LOW);
   digitalWrite(dirMotoG,HIGH);
+// Pour donner un acoup et lancer les moteurs
   analogWrite(vitMotoD, 255);
   analogWrite(vitMotoG, 255);
   delay(5);
+// Pour faire tourner les moteurs a la vitesse voulue
   analogWrite(vitMotoD, vitesse);
   analogWrite(vitMotoG, vitesse);
 }
 
 void stop()
 {
-  Serial.println("stop");
+  Serial.println("S'arête !");
+// Arête la rotation des moteurs
   analogWrite(vitMotoD, 0);
   analogWrite(vitMotoG, 0);
 }
 
 float distance() {
+  Serial.println("Mesure la Distance");
+
   float varDistance;
   digitalWrite(ultraTrig, LOW);
   delayMicroseconds(5);
